626e9a4b36107ddd5a00ed3c70762799fb7c0188
Roman Kuteiko <romankuteiko@gmail.com>
2026-01-12 17:40:18 +0200

    implement coin overview and trending coins ui functionality using the fetcher utility function

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import DataTable from '@/components/DataTable'
+import CoinOverview from '@/components/home/CoinOverview';
+import TrendingCoins from '@/components/home/TrendingCoins';
+import { fetcher } from '@/lib/coingecko.actions';
+import { cn, formatCurrency } from '@/lib/utils';
+import { TrendingDown, TrendingUp } from 'lucide-react';
+import { headers } from 'next/headers';
+import Image from 'next/image'
+import Link from 'next/link';
+import React, { Suspense } from 'react'
 
-const page = () => {
-  return <div></div>
+const page = async () => {
+  return (
+    <main className='main-container'>
+    <section className="home-grid">
+      <Suspense fallback={<div>Loading Overview...</div>}>
+        <CoinOverview/>
+      </Suspense>
+
+      <Suspense fallback={<div>Loading Trending...</div>}>
+        <TrendingCoins/>
+      </Suspense>
+    </section>
+
+    <section className="w-full mt-7 space-y-4">
+      <p>Categories</p>
+    </section>
+  </main>
+  )
 }
 
 export default page
